8ee29850c520531595411ffc94f68fce07073861
cao7113 <cao7113@hotmail.com>
2014-01-08 22:38:08 +0800

    new

--- etc/rbenv.d/install/ruby-build-essential.bash
+++ etc/rbenv.d/install/ruby-build-essential.bash
@@ -1,18 +1,16 @@
 #hooks for ruby-build or rbenv install to compile rubies 
 osinfo=`uname -a`
+script_dir=$(dirname $BASH_SOURCE)/../../../recipe #echo $BASH_SOURCE $0 #this is sourced
+
 if echo $osinfo|grep -iq ubuntu; then
   flag_file=~/.ruby-built-essential
   if [ ! -f $flag_file ];then
-    before_install "
-     echo ====running befor hook
-     sudo apt-get install -y build-essential autoconf libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
-     exit 1"
-    #touch $flag_file
+    before_install "`cat $script_dir/ruby-build-essential`"
   fi 
 else
   if [ -z "$RUBY_BUILD_SKIP_ESSENTIAL" ];then
     echo Not support platform: $osinfo 
-    echo You can add support in $0
+    echo You can add support in $BASH_SOURCE
     exit 1
   fi
 fi
