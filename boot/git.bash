47d4695ed2b0c4afc26df5ba2c005e0954c274b9
cao7113 <cao7113@hotmail.com>
2014-01-10 21:23:38 +0800

    fix

--- boot/git.bash
+++ boot/git.bash
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -e
-if [ "$1" != "-f" ];then
-  which git >/dev/null && echo Has installed git ... && exit 1
-else
-  shift
-fi
 
 sudo apt-get -y install git && echo Installing git ... 
-git config --global user.name ${1:-cao7113}
-git config --global user.email ${2:-cao7113@hotmail.com}
 
-etc_dir=$(dirname $0)/../etc
-ln -sb $etc_dir/etc/dot.gitconfig ~/.gitconfig && echo Make a local link to ~/.gitconfig
-ln -sb $etc_dir/etc/dot.gitignore ~/.gitignore && echo Make a local link to ~/.gitignore
-sudo apt-get -y install gitk
+etc_dir=$(readlink -f $(dirname $0)/../etc)
+ln -sb $etc_dir/dot.gitconfig ~/.gitconfig && echo Make a local link to ~/.gitconfig
+ln -sb $etc_dir/dot.gitignore ~/.gitignore && echo Make a local link to ~/.gitignore
+
+#git config --global user.name ${1:-cao7113}
+#git config --global user.email ${2:-cao7113@hotmail.com}
 echo end.
--- boot/gnome-vim.bash
+++ boot/gnome-vim.bash
@@ -3,13 +3,13 @@
 #* vim(tiny) not support system paste/copy, using vim-gnome instead
 
 set -e
+
+etc_dir=$(readlink -f $(dirname $0)/../etc)
+ln -sb $etc_dir/dot.vimrc ~/.vimrc && echo Make a link ~/.vimrc to $etc_dir/dot.vimrc
+[ "$1" = "--config" ] && exit 1
+
 which vim >/dev/null || sudo apt-get -y install vim-gnome && echo Installing vim-gnome ...
 which ctags >/dev/null || sudo apt-get -y install exuberant-ctags && echo Installing exuberant-ctags ...
-
-if [ ! -d ~/.vim/bundle ];then
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-  etc_dir=$(dirname $0)/../etc
-  ln -sb $etc_dir/dot.vimrc ~/.vimrc && echo Make a link ~/.vimrc to $etc_dir/dot.vimrc
-  vim +BundleInstall +qall
-fi
+[ -d ~/.vim/bundle ]||{ mkdir -p ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle;}
+vim +BundleInstall +qall
 echo set gnome-vim
