ab004e30d4a340615bca464b6b0032b8c51d7844
cao7113 <cao7113@hotmail.com>
2014-02-09 21:09:45 +0800

    fix nginx

--- boot/nginx-stable.bash
+++ boot/nginx-stable.bash
@@ -7,15 +7,14 @@ set -e
 
 echo latest at http://nginx.org/en/download.html
 echo http://nginx.org/en/linux_packages.html#stable
-sources_file=/etc/apt/sources.list
-grep -q nginx $sources_file && echo Installed nginx && exit 1 
+nginx_sources_list=/etc/apt/sources.list.d/nginx-stable.list
+[ -f $nginx_sources_list ] && echo Found $nginx_sources_list && exit 1 
 
 wget -O /tmp/nginx_signing.key http://nginx.org/keys/nginx_signing.key
 sudo apt-key add /tmp/nginx_signing.key
 
 ubuntu_codename=`lsb_release --codename --short` #e.g. precise for ubuntu 12.04LTS
-#TODO permission! into /etc/apt/sources.list.d?
-sudo cat<<-Note >>/etc/apt/sources.list
+sudo cat<<-Note >$nginx_sources_list
 #set nginx sources
 deb http://nginx.org/packages/ubuntu/ $ubuntu_codename nginx
 deb-src http://nginx.org/packages/ubuntu/ $ubuntu_codename nginx
