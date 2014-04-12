bceb6b4a18aaa1f8c8c7797009c9c35a93a969b3
cao7113 <cao7113@hotmail.com>
2014-04-12 14:43:46 +0800

    new

--- boot/rethinkdb.bash
+++ boot/rethinkdb.bash
@@ -4,6 +4,8 @@ set -e
 which add-apt-repository &>/dev/null ||{
   sudo apt-get -y update
   #easily add repo to apt eg. add-apt-repository ppa:nginx/stable
+  #Ubuntu Quantal and above (>= 12.10) — sudo apt-get install software-properties-common
+  #Earlier Ubuntu versions (< 12.10) — sudo apt-get install python-software-properties
   sudo apt-get -y install python-software-properties
 }
 
