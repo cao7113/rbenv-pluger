50eba70956037a850feed61af0caa6320bb8bb82
cao7113 <cao7113@hotmail.com>
2014-04-14 16:11:22 +0800

    add comment

--- boot/mongo-install.bash
+++ boot/mongo-install.bash
@@ -6,6 +6,13 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 mongo_source=/etc/apt/sources.list.d/mongodb.list
 [ -e $mongo_source ] ||{ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list; }
 sudo apt-get update
+
+# for 2.6.x
+#Install the latest stable version of MongoDB Enterprise.
+#sudo apt-get install mongodb-org
+#apt-get install mongodb-org=2.6.1 mongodb-org-server=2.6.1 mongodb-org-shell=2.6.1 mongodb-org-mongos=2.6.1 mongodb-org-tools=2.6.1
+
+#You cannot install this package concurrently with the mongodb, mongodb-server, or mongodb-clients packages provided by Ubuntu.
 sudo apt-get install mongodb-10gen
 #install a specific version
 # sudo apt-get install mongodb-10gen=2.2.3
@@ -16,10 +23,8 @@ configure:
  * data: /var/lib/mongo
  * log:  /var/log/mongo
  * sudo service mongodb start|stop|restart
-
  or manual installation:
-
    http://docs.mongodb.org/manual/tutorial/install-mongodb-on-linux/
-
    可安装多个版本，默认数据文件在/data/db
 Doc
+
