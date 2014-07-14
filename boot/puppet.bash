ae7560e3dde1ed6d129c73622232d8897723efa3
cao7113 <cao7113@hotmail.com>
2014-07-14 17:53:08 +0800

    new

--- boot/puppet.bash
+++ boot/puppet.bash
@@ -19,14 +19,20 @@ sudo apt-get update
 
 if [ "$1" = "master" ];then
   echo Step2: Install puppet on the Puppet Master Server, skip this for a standalone deployment
-  #sudo apt-get install puppetmaster-passenger #recommend with production-capacity web server
-  sudo apt-get -y install puppetmaster
-  echo Install puppet, its prerequisties and an init script at /etc/init.d/puppetmaster
-  #upgrade
+  #sudo apt-get install puppetmaster-passenger #recommended with production-capacity web server using Passenger+Apache2
+  sudo apt-get -y install puppetmaster #use Webrick as the default web server, just for test; require config a better rack-based server!
+  echo Install puppet, its prerequisties and an init script at /etc/init.d/puppetmaster for master daemon!
+  #when upgrade
   #sudo apt-get update
-  #sudo puppet resource package puppet ensure=latest
+  #sudo puppet resource package puppetmaster ensure=latest
 fi
 
 echo Install Puppet on Agent nodes or standalone mode
 sudo apt-get -y install puppet
-echo install puppet and init script at /etc/init.d/puppet
+#when upgrade
+#sudo apt-get update
+#sudo puppet resource package puppet ensure=latest
+echo install puppet and init script at /etc/init.d/puppet for agent daemon
+
+#post-install
+echo configure for agent/master mode by http://docs.puppetlabs.com/guides/install_puppet/post_install.html
