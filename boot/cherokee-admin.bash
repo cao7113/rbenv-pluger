d46b0e6788097f537de6c68bf4887e5a1a32100e
cao7113 <cao7113@hotmail.com>
2014-08-25 17:20:07 +0800

    by pman

---
+++ boot/cherokee-admin.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+set -e
+
+sudo cherokee-admin -b --unsecure
+# admin port at 9090
+echo Ok
---
+++ boot/uwsgi-emperor.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+#Created from mksh on 2014年 08月 25日 星期一 12:27:41 CST
+
+user=${1:-$USER}
+uwsgi_bin=`which uwsgi`
+vassals_dir=/etc/uwsgi
+sudo mkdir -p $vassals_dir
+
+cat <<-Script >/tmp/emperor.conf
+description "uWSGI Emperor Job"
+start on runlevel [2345]
+stop on runlevel [06]
+setuid $user
+setgid $user
+exec $uwsgi_bin --die-on-term --emperor $vassals_dir --logto /var/log/emperor.log
+Script
+
+sudo mv -b /tmp/emperor.conf /etc/init
+echo Write conf file to /etc/init/emperor.conf
