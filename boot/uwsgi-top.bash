9d614783f29b807c1cc39fa2d84a509586b9768a
cao7113 <cao7113@hotmail.com>
2014-08-25 12:29:29 +0800

    new

---
+++ boot/uwsgi-top.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+#Created from mksh on 2014年 08月 25日 星期一 12:27:41 CST
+
+echo Ref uwsgitop tool https://github.com/unbit/uwsgitop
+sudo apt-get -y install python-pip
+sudo pip install uwsgitop
+echo If your stats server at :3002, run by uwsgitop :3002
