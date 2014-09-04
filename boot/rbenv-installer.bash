13327b9c001cf141c4ed0157a92d237e5b188306
cao7113 <cao7113@hotmail.com>
2014-09-04 10:02:22 +0800

    new

--- boot/rbenv-installer.bash
+++ boot/rbenv-installer.bash
@@ -5,4 +5,4 @@ rbenv_recipe=$(dirname $0)/../recipe/rbenv
 #RUBY_BUILD_MIRROR_URL=http://ruby.taobao.org/mirrors/ruby/ RUBY_VERSION=${1:-2.1.0} $rbenv_recipe --debug 
 RUBY_BUILD_MIRROR_URL=http://ruby.taobao.org/mirrors/ruby/ CONFIGURE_OPTS="--disable-install-doc" RUBY_VERSION=${1:-2.0.0-p481} $rbenv_recipe --debug 
 
-echo Note: 2.1.0/1 has bug with readline ref doc/bugs.md, recommend 2.1.2!
+echo Note: 2.1.0/1 has bug with readline ref doc/bugs.md, please use 2.1.2 instead!
