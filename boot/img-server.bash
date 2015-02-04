f49cee45c8baf5ce656c53ec0d91106dfc323f4a
cao7113 <cao7113@hotmail.com>
2015-02-04 17:07:25 +0800

    new

--- boot/img-server.bash
+++ boot/img-server.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 set -e
-echo to install rmagick gem?
-sudo apt-get install -y imagemagick librmagick-ruby libmagickcore-dev libmagickwand-dev
+echo "to install mini_magick/rmagick(deprecated) gem?"
+sudo apt-get install -y imagemagick libmagickwand-dev 
