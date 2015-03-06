7c3af47311814d1eae0dccc09a3eb095a80543b5
cao7113 <cao7113@hotmail.com>
2015-03-06 18:54:55 +0800

    new

--- boot/os.bash
+++ boot/os.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
-sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt-get -y update 
+#sudo apt-get -y upgrade
+
 #easily add repo to apt eg. add-apt-repository ppa:nginx/stable
-sudo apt-get -y install python-software-properties 
+#sudo apt-get -y install python-software-properties 
